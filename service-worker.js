2f59b093a6b686f95b7074bdd4024aed24c28aa7
niki0123456789 <chairblue07@gmail.com>
2026-07-18 20:22:45 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,16 @@
 "use strict";
-const CACHE_NAME = "equal-love-photo-manager-public-v1004";
+const CACHE_NAME = "equal-love-photo-manager-public-v1006";
 const APP_SHELL = [
   "./",
   "./index.html",
-  "./css/style.css?v=1.00.4",
-  "./js/app.js?v=1.00.4",
-  "./js/bootstrap.js?v=1.00.4",
-  "./data/events.json?v=1.00.4",
-  "./data/members.json?v=1.0.0",
+  "./css/style.css?v=1.00.6",
+  "./js/app.js?v=1.00.6",
+  "./js/bootstrap.js?v=1.00.6",
+  "./data/events.json?v=1.00.6",
+  "./data/members.json?v=1.00.6",
   "./data/positions.json?v=1.0.0-orderfix",
-  "./data/config.json?v=1.00.4",
-  "./manifest.webmanifest?v=1.0.4",
+  "./data/config.json?v=1.00.6",
+  "./manifest.webmanifest?v=1.0.6",
   "./icons/icon-192.png",
   "./icons/icon-512.png",
   "./icons/apple-touch-icon.png"
